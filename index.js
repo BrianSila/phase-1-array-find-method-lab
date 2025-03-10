2b173d67c212e6d3a8b88dd2920cd9a58993a433
Brian <brian.sila1@student.moringaschool.com>
2025-03-10 19:11:38 +0300

    phase-1-array-find-method-lab

--- index.js
+++ index.js
@@ -1,1 +1,13 @@
 // code your solution here
+function superbowlWin(array){
+    let Win = array.find(function(e){
+        return e.result === "W"
+    }
+    )
+    if(Win){
+        return Win.year
+    }
+    else{
+        return undefined
+    }
+}
